613cc1182b55999a3fcbb2f9b8babf2eccc12531
Marcus Stenbeck <marcus.stenbeck@gmail.com>
2022-09-04 09:13:33 +0200

    feat: add remotion player to Next.js page

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,7 +1,9 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Player } from '@remotion/player';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import { MyComposition } from '../remotion/MyComposition';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
@@ -14,42 +16,30 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <span className="text-red-500">Welcome</span> to{' '}
+          <a href="https://nextjs.org">Next.js</a>
         </h1>
-
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        {/* I've added a dashed red border so we can see the edges of the video. */}
+        <div className="border-2 border-dashed border-red-500">
+          <Player
+            style={{
+              // The remotion player will resize to fit inside the player.
+              // We set a fixed width and the height will be automatically adjusted for you.
+              width: 640,
+            }}
+            component={MyComposition}
+            durationInFrames={120}
+            compositionWidth={1920}
+            compositionHeight={1080}
+            fps={30}
+            loop
+            autoPlay
+            controls
+          />
         </div>
       </main>
 
@@ -66,7 +56,7 @@ const Home: NextPage = () => {
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
